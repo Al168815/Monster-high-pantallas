df82bc3ec18cfa855243c6af26ec5b19dedad68d
Al168815 <116653962+Al168815@users.noreply.github.com>
2024-11-15 10:43:28 -0700

    FALTA PANTALLA ARI

---
+++ monsterHigh/MonsterhIGH/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  MonsterhIGH
+//
+//  Created by alumno on 11/8/24.
+//
+
+import UIKit
+ 
+class ViewController: UIViewController {
+override func viewDidLoad() {
+    super.viewDidLoad()
+
+    
+}
+
+    @IBAction func VerPersonajes(_ sender: Any) {
+        performSegue(withIdentifier: "ViewControllerLista", sender: self)
+    }
+    @IBAction func CrearPersonaje(_ sender: Any) {
+        performSegue(withIdentifier: "MonsterFormViewController", sender: self)
+    }
+    @IBAction func Noticias(_ sender: Any) {
+        performSegue(withIdentifier: "NoticiasViewController", sender: self)
+        
+    }
+}
+
